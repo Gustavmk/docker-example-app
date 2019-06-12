bd5f2c83807bfef48386f719d309c08ce652ea35
Rafael Dalsenter <rafaeldalsenter@gmail.com>
2019-06-12 18:43:51 -0300

    🐞 Correções de bugs

--- Docker.Example.App/Services/CadastroService.cs
+++ Docker.Example.App/Services/CadastroService.cs
@@ -12,10 +12,10 @@ namespace Docker.Example.App.Services
             {
                 RodarMigrations();
 
-                using (var conn = new SqlConnection("Server=container-sql;Database=master;User Id=sa;Password=docker12345;"))
+                using (var conn = new SqlConnection("Server=container-sql;Database=master;User Id=sa;Password=Docker12345;"))
                 {
                     conn.Open();
-                    var cmd = new SqlCommand($"INSERT INTO cadastro VALUES ({model.Nome}, {model.Telefone})", conn);
+                    var cmd = new SqlCommand($"INSERT INTO cadastro VALUES ('{model.Nome}', '{model.Telefone}')", conn);
                     cmd.ExecuteNonQuery();
                     model.MensagemDeRetorno = "PARABENS!";
                 }
@@ -30,7 +30,7 @@ namespace Docker.Example.App.Services
         {
             try
             {
-                using (var conn = new SqlConnection("Server=container-sql;Database=master;User Id=sa;Password=docker12345;"))
+                using (var conn = new SqlConnection("Server=container-sql;Database=master;User Id=sa;Password=Docker12345;"))
                 {
                     conn.Open();
                     var cmd = new SqlCommand($"CREATE TABLE cadastro (nome VARCHAR(100), telefone VARCHAR(100)) ", conn);
